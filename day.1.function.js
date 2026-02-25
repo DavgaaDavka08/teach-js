fc9c916a9ad2ed603ab64ab4d7dc13f62cb63188
DavgaaDavka08 <iderbatdagvadorj@gmail.com>
2026-02-25 16:29:37 +0800

    done

--- day.1.function.js
+++ day.1.function.js
@@ -1,8 +1,3 @@
-//heseg buleg davtagddg code iig copy past hiigeed heretei gazraa ashiglaad yvdag
-//javascript function zarchim code maintance olon code iig baga bichih
-//DRY - dont repead yourself -bitgii code davtaj bich
-//() dotor function prametter buyu damjulah ogogdol bichne
-
 // function say(name, age, score) {
 //   console.log("name :>> ", name);
 //   console.log("age :>> ", age);
@@ -39,8 +34,75 @@
 // let result = hello("hello world");
 // console.log(result);
 
-function student(a, b) {
-  return a + b;
-}
-let result = student(10, 50);
-console.log("result :>> ", result);
+// function student(a, b) {
+//   return a + b;
+// }
+// let result = student(10, 50);
+// console.log("result :>> ", result);
+
+// function students(name, age, score) {
+//   console.log("name :>> ", name);
+//   console.log("age :>> ", age);
+//   console.log("score :>> ", score);
+// }
+// students("bat", 17, 80);
+// students("bolor", 18, 80);
+
+// function numberModule(a) {
+//   console.log("numberModule function ajilla :>> ");
+//   return a * 2;
+// }
+
+// function num() {
+//   console.log("num function ajilla :>> ");
+//   numberModule();
+// }
+// let result = numberModule(10);
+// console.log("result :>> ", result);
+//
+//
+//heseg buleg davtagddg code iig copy past hiigeed heretei gazraa ashiglaad yvdag
+//javascript function zarchim code maintance olon code iig baga bichih
+//DRY - dont repead yourself -bitgii code davtaj bich
+//() dotor function prametter buyu damjulah ogogdol bichne
+//JavaScript-д функц нь тодорхой нэгэн үүрэг гүйцэтгэх кодын
+//бүлэг бөгөөд дахин ашиглах боломжийг олгодог.
+
+// function isEven(num) {
+//   if (num % 2 == 0) {
+//     return "tegsh";
+//   } else {
+//     return "sondgoi";
+//   }
+// }
+
+// let result = isEven(-50);
+// console.log("result :>> ", result);
+
+// function Hello() {
+//   console.log("hello worl");
+// }
+
+// Hello();
+
+//
+
+// function Student(age, name, score) {
+//   console.log("age :>> ", age);
+//   console.log("name :>> ", name);
+//   console.log("score :>> ", score);
+// }
+// Student("davgaa", 10, 10);
+
+// function Student(a, b, c) {
+//   console.log("a :>> ", a);
+//   console.log("b :>> ", b);
+//   console.log("c :>> ", c);
+// }
+// Student(10, 1, 1);
+
+let name = "bat";
+let age = 20;
+let score = 60;
+
+function student() {}
